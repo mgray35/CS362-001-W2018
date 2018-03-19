2becd4f2513a16a6636c08da69e5d7b42c846fd9
McKenzie <mac.gray.311@gmail.com>
2018-03-19 11:48:59 -0700

    added some manual tests

--- projects/kooske/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/kooske/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -13,38 +13,44 @@ import junit.framework.TestCase;
 
 public class UrlValidatorTest extends TestCase {
 
-
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
-   
-   
+   public static void main(String[] argv) {
+      UrlValidatorTest urlValidatorTest = new UrlValidatorTest("url test");
+      urlValidatorTest.testManualTest();
+   }
+
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+      UrlValidator urlValidator = new UrlValidator();
+      assertFalse(urlValidator.isValid("asaodkfsdl"));
+//      assertTrue(urlValidator.isValid("www.google.com"));
+//      assertTrue(urlValidator.isValid("https://en.wikipedia.org/"));
+//      assertTrue(urlValidator.isValid("facebook.com"));
+      assertFalse(urlValidator.isValid("http//ww.badurl,net"));
    }
-   
-   
+
+
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+      //You can use this function to implement your First Partition testing
 
    }
-   
+
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+      //You can use this function to implement your Second Partition testing
 
    }
    //You need to create more test cases for your Partitions if you need to 
-   
+
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+      //You can use this function for programming based testing
 
    }
-   
+
 
 
 }
